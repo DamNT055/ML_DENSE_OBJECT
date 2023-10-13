44ca4101e6670c1562efd3ffd302ea8448490bf0
damnguyen.tga@gmail.com <89400030+DamNT055@users.noreply.github.com>
2023-10-13 14:42:50 +0700

    commit change

--- src/vision_utils/engine.py
+++ src/vision_utils/engine.py
@@ -61,8 +61,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-        #writer.add_scalar('training loss', losses_reduced,
-                          "epoch {}".format(epoch))
+        #writer.add_scalar('training loss', losses_reduced, "epoch {}".format(epoch))
         #writer.add_scalar('learning rate', optimizer.param_groups[0]["lr"])
 
     return metric_logger
